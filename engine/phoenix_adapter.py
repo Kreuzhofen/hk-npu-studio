8b114ffb3c118e0555554151431513b15d067777
Holger Kreuzhofen <holger.kreuzhofen@outlook.de>
2026-06-27 15:50:48 +0200

    Sprint 012.5: add PhoenixAdapter for GUI integration

---
+++ engine/phoenix_adapter.py
@@ -0,0 +1,26 @@
+"""
+SnapdragonAI Studio
+
+Phoenix Adapter
+
+Created by Holger Kreuzhofen
+Phoenix Engine
+"""
+
+from engine.phoenix_core import PhoenixCore
+
+
+class PhoenixAdapter:
+    """
+    Adapter zwischen GUI und Phoenix Engine.
+    """
+
+    def __init__(self):
+        self.core = PhoenixCore()
+
+    def run(self, skill: str, **kwargs):
+        """
+        Führt einen Skill über die Phoenix Engine aus.
+        """
+
+        return self.core.run(skill, **kwargs)
