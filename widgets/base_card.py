aad3f55aa29d8c6a4d2f78b03cba4476c7173757
Holger Kreuzhofen <holger.kreuzhofen@outlook.de>
2026-06-28 08:57:30 +0200

    Sprint 014.0: add BaseCard widget foundation

---
+++ widgets/base_card.py
@@ -0,0 +1,27 @@
+"""
+SnapdragonAI Studio
+
+Base Card Widget
+
+Created by Holger Kreuzhofen
+Phoenix UI
+"""
+
+from tkinter import ttk
+
+
+class BaseCard(ttk.LabelFrame):
+    """
+    Gemeinsame Basisklasse für alle Karten-Widgets
+    im SnapdragonAI Studio.
+    """
+
+    def __init__(self, master, title: str):
+        super().__init__(master, text=title)
+
+    def set_title(self, title: str):
+        """
+        Ändert den Titel der Karte.
+        """
+
+        self.configure(text=title)
